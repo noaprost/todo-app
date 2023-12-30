616b388249bdffc15cd7c5647eaf8946742b6776
noaprost <kangjiwon1850@gmail.com>
2023-12-30 23:53:47 +0900

    localStorage 할일 업데이트 오류 수정

--- src/context/ToDoContext.jsx
+++ src/context/ToDoContext.jsx
@@ -3,23 +3,12 @@ import { createContext, useContext, useEffect, useState } from "react";
 const ToDoContext = createContext();
 
 export function ToDoProvider({ children }) {
-  const [todos, setTodos] = useState([
-    { id: "", text: "할 일을 추가해주세요", status: "active" },
-  ]);
+  const [todos, setTodos] = useState([]);
   const handleTodos = (newTodos) => {
     setTodos(newTodos);
-    localStorage.todos = todos;
   };
 
-  console.log(localStorage.todos);
-
-  useEffect(() => {
-    if (!("todos" in localStorage)) {
-      setTodos(localStorage.todos);
-    } else {
-      localStorage.setItem("todos", todos);
-    }
-  }, []);
+  useEffect(() => {}, []);
   return (
     <ToDoContext.Provider value={{ todos, handleTodos }}>
       {children}
